b7256d559909e235f75eb4f779f75d4d205d45ba
menaka92 <127770422+menaka92@users.noreply.github.com>
2023-04-27 00:20:52 +0530

    Update fv.py

--- fv.py
+++ fv.py
@@ -1,3 +1,99 @@
+past_calculations = []
+
+
+def add(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    try:
+        return a/b
+    except Exception as e:
+        print(e)
+
+
+def power(a, b):
+    return a**b
+
+
+def reminder(a, b):
+    return a % b
+
+
+def history():
+    if past_calculations:
+        for index, calc in enumerate(past_calculations):
+            print(calc)
+    else:
+        print("No past calculations to show")
+        return 0
+
+
+def select_op(choice):
+    if (choice == "?"):
+        return history()
+    if (choice == "#"):
+        return -1
+    elif (choice in ('+', '-', '*', '/', '%')):
+        while (True):
+            num1s = str(input("enter first number: "))
+            print(num1s)
+            if num1s.endswith('$'):
+                return 0
+            if num1s.endswith('#'):
+                return -1
+
+            try:
+                num1 = float(num1s)
+                break
+            except:
+                print("not a valid number,please enter again")
+                continue
+
+        while (True):
+            num2s = str(input("Enter second number: "))
+            print(num2s)
+            if num2s.endswith('$'):
+                return 0
+            if num2s.endswith('#'):
+                return -1
+            try:
+                num2 = float(num2s)
+                break
+            except:
+                print("not a valid number , please enter again")
+                continue
+
+    result = 0.0
+    last_calculation = ""
+    if choice == '+':
+        result = add(num1, num2)
+    elif choice == '-':
+        result = sub(num1, num2)
+    elif choice == '*':
+        result = mul(num1, num2)
+    elif choice == '/':
+        result = div(num1, num2)
+    elif choice == '^':
+        result = power(num1, num2)
+    elif choice == '%':
+        result = reminder(num1, num2)
+    else:
+        print("something went wrong")
+
+    last_calculation = "{0} {1} {2} = {3}".format(num1, choice, num2, result)
+    print(last_calculation)
+    past_calculations.append(last_calculation)
+
+
 while True:
     print("Select operation.")
     print("1.Add: + ")
@@ -8,45 +104,11 @@ while True:
     print("6.Remainder: % ")
     print("7.Terminate: # ")
     print("8.Reset: $ ")
-    
-    a = float(input("Enter 1st number: "))
-    b  = float(input("Enter 2nd number: "))
-    choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
 
-    def add(a,b):
-        return(a+b)
-    def sub(a,b):
-        return(a-b)
-    def multy(a,b):
-        return(a*b)
-    def dev(a,b):
-        if(b==0):
-            print("Not a valid number,please enter again")
-            return None
-        else:
-            return(a/b)
-    def power(a, b):
-        return a ** b
-    def remainder(a, b):
-        if b == 0:
-            print("Not a valid number,please enter again")
-            return None
-        else:
-            return (a % b)
+    choice = input("Enter choice ('+', '-', '*', '/', '%'): ")
+    print(choice)
 
-    if(choice=='+'):
-        print (add(a,b))
-    if(choice=='-'):
-        print (sub(a,b))
-    if(choice=='*'):
-        print (multy(a,b))
-    if(choice=='/'):
-        print (dev(a,b))
-    if(choice=='^'):
-        print (power(a,b))
-    if(choice=='%'):
-        print (remainder(a,b))
-    if(choice=='#'):
-        break
-    if(choice=='$'):
-        continue
+    if select_op(choice) == -1:
+        print("done. terminating")
+       
+
